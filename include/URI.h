990c20e024fce989f6a0377ec5340fbae2493ac7
Cristian Musat <cristutz00@gmail.com>
2024-11-29 12:40:41 +0200

    fix mongo uri

--- include/URI.h
+++ include/URI.h
@@ -1,6 +1,6 @@
 #ifndef URI_H
 #define URI_H
 
-#define MONGO_URI mongodb+srv://cristutz00:QL4OxeZM3Sf0csjo@c-project-cluster.dl897.mongodb.net/?retryWrites=true&w=majority&appName=C-project-cluster
+#define MONGO_URI "mongodb+srv://cristutz00:QL4OxeZM3Sf0csjo@c-project-cluster.dl897.mongodb.net/?retryWrites=true&w=majority&appName=C-project-cluster"
 
 #endif
